83bb48fb182d3be71763ed5d483387ed436be618
Vicent Tsai <vayn@vayn.de>
2016-12-10 13:44:06 +0800

    Update
    Use can choose whether installing plugins after initialization.

--- install.sh
+++ install.sh
@@ -1,30 +1,90 @@
+#!/bin/sh
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [[ -t 1 ]] && [[ -n $ncolors ]] && [[ $ncolors -ge 8 ]]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+# Only enable exit-on-error after the non-critical colorization stuff,
+# which may fail on systems lacking tput or terminfo
+set -e
+
+
+###
+# Helpers {{{1
+###
+
+init() {
+    local RC=${1}
+    local DF_RC=${2}
+
+    if [[ $# -eq 4 ]]; then
+        local DIR=${3}
+        local DF_DIR=${4}
+    fi
+
+    if [[ -f $RC ]] || [[ -h $RC ]]; then
+        printf "${BLUE}Looking for an existing config...${NORMAL}\n"
+
+        printf "${YELLOW}Found $RC${NORMAL}\n"
+        printf "${GREEN}Backing up to $RC.pre-dotfiles${NORMAL}\n"
+        mv $RC $RC.pre-dotfiles
+    fi
+    printf "${BLUE}Linking $RC...${NORMAL}\n\n"
+    ln -s $DF_RC $RC
+
+    if [[ ! -z $DIR ]]; then
+        if [[ -d $DIR ]]; then
+            printf "${BLUE}Looking for an existing config directory...${NORMAL}\n"
+
+            printf "${YELLOW}Found $DIR${NORMAL}\n"
+            printf "${GREEN}Backing up to $DIR.pre-dotfiles${NORMAL}\n"
+            mv $DIR $DIR.pre-dotfiles
+        fi
+
+        printf "${BLUE}Linking $DF_DIR...${NORMAL}\n\n"
+        ln -s $DF_DIR $DIR
+    fi
+}
+
+confirm () {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case $response in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+# End: Helpers 1}}}
+
+
+###
+# Main {{{1
+###
+
 main() {
     local DOTFILES=~/.dotfiles
-
-    # Use colors, but only if connected to a terminal, and that terminal
-    # supports them.
-    if which tput >/dev/null 2>&1; then
-        ncolors=$(tput colors)
-    fi
-    if [[ -t 1 ]] && [[ -n $ncolors ]] && [[ $ncolors -ge 8 ]]; then
-        RED="$(tput setaf 1)"
-        GREEN="$(tput setaf 2)"
-        YELLOW="$(tput setaf 3)"
-        BLUE="$(tput setaf 4)"
-        BOLD="$(tput bold)"
-        NORMAL="$(tput sgr0)"
-    else
-        RED=""
-        GREEN=""
-        YELLOW=""
-        BLUE=""
-        BOLD=""
-        NORMAL=""
-    fi
-
-    # Only enable exit-on-error after the non-critical colorization stuff,
-    # which may fail on systems lacking tput or terminfo
-    set -e
 
     local PLATFORM='unknown'
     if which uname >/dev/null 2>&1; then
@@ -86,45 +146,31 @@ main() {
     # Tmux Initialization
     local DF_TMUXCONF=$DOTFILES/_tmux.conf
     init ~/.tmux.conf $DF_TMUXCONF
+
+    # Plugins Installation {{{2
+    printf "${YELLOW}Initialization done.${NORMAL}\n\n"
+
+    printf "${BOLD}Installing Zsh plugins...${NORMAL}\n"
+    if [[ $(confirm) ]]; then
+        pip install zsh-directory-history 
+    else
+        printf "${GREEN}You can install Zsh plugins manually:${NORMAL}\n\n"
+        printf "  ${RED}\`pip install zsh-directory-history\`${NORMAL}\n\n"
+    fi
+
+
+    printf "${BOLD}Installing Vim plugins...${NORMAL}\n"
+    if [[ $(confirm) ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh)"
+        vim +NeoBundleInstall +qall
+    else
+        printf "${GREEN}You can install Vim plugins manually:${NORMAL}\n\n"
+        printf "  ${RED}\`sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh)\"\`${NORMAL}\n"
+        printf "  ${RED}\`vim +NeoBundleInstall +qall\`${NORMAL}\n\n"
+    fi
+    # End: Plugins Installation 2}}}
 }
 
-init() {
-    local RC=${1}
-    local DF_RC=${2}
-
-    if [[ $# -eq 4 ]]; then
-        local DIR=${3}
-        local DF_DIR=${4}
-    fi
-
-    if [[ -f $RC ]] || [[ -h $RC ]]; then
-        printf "${BLUE}Looking for an existing config...${NORMAL}\n"
-
-        printf "${YELLOW}Found $RC${NORMAL}\n"
-        printf "${GREEN}Backing up to $RC.pre-dotfiles${NORMAL}\n"
-        mv $RC $RC.pre-dotfiles
-    fi
-    printf "${BLUE}Linking $RC...${NORMAL}\n\n"
-    ln -s $DF_RC $RC
-
-    if [[ ! -z $DIR ]]; then
-        if [[ -d $DIR ]]; then
-            printf "${BLUE}Looking for an existing config directory...${NORMAL}\n"
-
-            printf "${YELLOW}Found $DIR${NORMAL}\n"
-            printf "${GREEN}Backing up to $DIR.pre-dotfiles${NORMAL}\n"
-            mv $DIR $DIR.pre-dotfiles
-        fi
-
-        printf "${BLUE}Linking $DF_DIR...${NORMAL}\n\n"
-        ln -s $DF_DIR $DIR
-    fi
-}
+# End: Main 1}}}
 
 main
-
-printf "${YELLOW}Done.${NORMAL} Now you should use command below to install Vim and Zsh plugins:\n\n"
-printf "$ ${RED}pip install zsh-directory-history${NORMAL}\n"
-printf "$ ${RED}sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh)\"${NORMAL}\n"
-printf "$ ${RED}vim +NeoBundleInstall +qall${NORMAL}\n\n"
-
